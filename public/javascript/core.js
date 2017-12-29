ce5db63b619ee04d15112f3efc6bb4131d05694c
Joe Holt <wynatoej@gmail.com>
2017-12-29 16:33:58 -0600

    Added WIP ui for orders page

--- public/javascript/core.js
+++ public/javascript/core.js
@@ -1,5 +1,6 @@
 // Variables ===================================================================
 var display = angular.module('SpartanManApp', []);
+var orders;
 
 // Controller ==================================================================
 var dataController = function mainController($scope, $http){
@@ -9,6 +10,7 @@ var dataController = function mainController($scope, $http){
     url: '/api/getorders'
   }).then(function(result) {
     $scope.orders = result.data
+    orders = result.data
   }, function(error) {
     console.log(error);
   });
@@ -31,6 +33,23 @@ var dataController = function mainController($scope, $http){
   }, function(error) {
     console.error(error);
   });
+
+  $scope.delete = function(index) {
+    let id = orders[index].id
+    console.log(id);
+
+    $http({
+      method: 'POST',
+      url: '/api/deleteorder',
+      data: $.param({ 'id': id }),
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    }).then(function(result) {
+      location.reload();
+    }, function(error) {
+      console.error(error);
+    });
+
+  }
 
 }
 
