0a1cd5aa77358088e56825e65f2173a68664e5a0
Joe Holt <wynatoej@gmail.com>
2017-12-30 14:52:14 -0600

    Added welcome popup

--- public/javascript/universal.js
+++ public/javascript/universal.js
@@ -1,3 +1,11 @@
 $.get("navigation.html", function(data) {
   $("#nav-placeholder").replaceWith(data);
 })
+$(document).ready(function() {
+    var isshow = localStorage.getItem('isshow');
+    if (isshow== null || isshow == 0) {
+        localStorage.setItem('isshow', 1);
+        // Show popup here
+        alert("Welcome to the Spartan Manufacturing Hub!\nPlease report any issues you have to me (Joe Holt), thank you!");
+    }
+});
